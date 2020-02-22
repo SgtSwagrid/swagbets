792712920111993f1bae3467f5b7557e8affed60
Alec Dorrington <alec.dorrington@protonmail.com>
2020-02-22 17:22:07 +0800

    Changed settings for Heroku deployment.

--- swagbets/settings.py
+++ swagbets/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -30,7 +31,9 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     'swagbets.net',
-    'www.swagbets.net'
+    'www.swagbets.net',
+    'swagbets/herokuapp.com',
+    'www.swagbets/herokuapp.com'
 ]
 
 
@@ -99,6 +102,9 @@ DATABASES = {
     }
 }
 
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
